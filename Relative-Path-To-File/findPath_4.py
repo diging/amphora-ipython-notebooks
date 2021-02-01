10822234eb7ac7dc6d7073407b7ccb599e938447
Navaneesh Kumar <navaneeshkumarb@gmail.com>
2021-02-01 16:21:28 -0700

    [RS-1] Improve matching logic

--- Relative-Path-To-File/findPath_4.py
+++ Relative-Path-To-File/findPath_4.py
@@ -1,3 +1,4 @@
+import re
 import pandas as pd
 from math import isnan
 from pathlib import Path
@@ -26,20 +27,20 @@ class Sheet:
                 else:
                     break
 
-            self.articles.append({
-                "title": title,
-                "pmid": pmid,
-                "pmcid": pmcid,
-                "year": year,
-                "authors": authors
-            })
-            if i == 10: break
+            if title:
+                self.articles.append({
+                    "title": title,
+                    "pmid": pmid,
+                    "pmcid": pmcid,
+                    "year": year,
+                    "authors": authors
+                })
 
 class Journal:
     def __init__(self):
-        self.metadata = "/Users/nowke/Desktop/diging/metadata"
-        self.folder = "/Users/nowke/Desktop/diging/files/"
-        self.destination = "/Users/nowke/Desktop/diging/not_found"
+        self.metadata = "/Users/nowke/Documents/diging/metadata"
+        self.folder = "/Users/nowke/Documents/diging/files/"
+        self.destination = "/Users/nowke/Documents/diging/not_found"
         self.files = []
         self.meta_files = []
 
@@ -52,6 +53,7 @@ class Journal:
         folder_path = Path(self.folder)
         text_files = list(folder_path.glob("**/*.txt"))
         self.files = text_files
+        print(f"Found {len(self.files)} inside '{folder_path}'")
 
     def get_metadata_files(self):
         folder_path = Path(self.metadata)
@@ -59,32 +61,62 @@ class Journal:
         self.meta_files = meta_files
 
     def perform_match(self):
+        print(f"Scanning metadata - '{self.meta_files[0]}'")
+        
         sheet = Sheet(self.meta_files[0])
-        text_file = self.files[657]
-        article_meta = sheet.articles[0]
+        print(f"Found {len(sheet.articles)} inside the metadata sheet\n")
 
+        for i, text_file in enumerate(self.files):
+            print(f"Scanning File ({i + 1}/{len(self.files)}) {text_file}")
+            for article_meta in sheet.articles:
+                score = self.get_score_for_text(sheet, text_file, article_meta)
+                # if score >= 30:
+                #     print("===============")
+                #     print(f"Matching {text_file} with {article_meta['title']}")
+                #     print(score)
+                #     print("===============")
+                #     print()
+                if score >= 80:
+                    print("Matched all")
+                    break
+
+    def get_score_for_text(self, sheet, text_file, article_meta):
         # Match contents of `text_file` and `article_meta`
-        with open(text_file) as f:
+        with open(text_file, encoding='utf8') as f:
             size_to_read = self.get_file_size_to_read(text_file)
             data = f.read(size_to_read)
-            print(article_meta["title"])
-            score = 0
-            if article_meta["title"] and article_meta["title"] in data:
-                score += 50
-            if article_meta["pmid"] and str(article_meta["pmid"]) in data:
-                score += 10
-            if article_meta["pmcid"] and str(article_meta["pmcid"]) in data:
-                score += 10
-            author_matches = 0
-            for author in article_meta["authors"]:
-                name = f"{author[1]} {author[0]}"
-                if name in data:
-                    author_matches += 1
+            data_cleaned = data.replace("’", "'").replace("-", "‐")
+            pmid = self._parse_article_field(article_meta["pmid"])
+            pmcid = self._parse_article_field(article_meta["pmcid"])
+            title = article_meta["title"].replace("’", "'").replace("-", "‐")
 
-            author_match_percentage = author_matches / len(article_meta["authors"])
-            if author_match_percentage == 1:
-                score += 30
-            print(score)
+            score = 0
+            if title and title.replace(" ", "").lower() in re.sub(r"[\s]", "", data_cleaned.replace(" ", "").lower()):
+                score += 50
+            if pmid and pmid in data:
+                score += 10
+            if pmcid and pmcid in data:
+                score += 10
+
+            if len(article_meta["authors"]) > 0:
+                # Match all authors
+                author_matches = 0
+                for author in article_meta["authors"]:
+                    last_name = author[1].strip().replace("’", "'").replace("-", "‐")
+                    first_name = author[0].strip().replace("’", "'").replace("-", "‐")
+                    name = f"{last_name} {first_name}"
+                    if name in data_cleaned:
+                        author_matches += 1
+
+                author_match_percentage = author_matches / len(article_meta["authors"])
+                score += 30 * author_match_percentage
+
+            # if score >= 30:
+            #     print("Authors", article_meta["authors"])
+            #     print(f"PMID {pmid}, PMCID {pmcid}")
+            
+            return score
+
 
     def get_file_size_to_read(self, text_file):
         size = text_file.stat().st_size
@@ -94,6 +126,14 @@ class Journal:
             return int(size * 2 / 3)
         else:
             return int(size / 3)
+
+    def _parse_article_field(self, field):
+        if type(field) is float and not isnan(field):
+            return str(int(field))
+        if type(field) is str:
+            return field
+        if type(field) is int:
+            return str(field)
 
 if __name__ == "__main__":
     journal = Journal()
